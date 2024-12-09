b043c49ad3d3048786db96c4230a2585835f68b1
Phillip Dykman <phil.d324@gmail.com>
2024-12-09 15:38:48 -0800

    fix:(checker) check for updates once a week

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,8 +26,13 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  install = { colorscheme = { "tokyonight" } },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    notify = true,
+    frequency = 604800 -- weekly
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
